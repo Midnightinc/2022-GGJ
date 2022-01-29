97e46ac6a1ca210dbed77ac0bea8de31d54ac7c9
Blake <42333817+Midnightinc@users.noreply.github.com>
2022-01-29 12:56:10 +1100

    Fixed some jank code on attack controller and added resource folder dependency for james

--- GGJ2022/Assets/Scripts/AttackController.cs
+++ GGJ2022/Assets/Scripts/AttackController.cs
@@ -1,97 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using AbilitySystem;
 using UnityEngine;
-using UnityEngine.UI;
-
-public class AttackController : MonoBehaviour
-{
-    private Action Shoot;
-
-    private float firerate = 8f;
-    private float nextTimetoFire = 0.5f;
-
-    private int currentBullets = 0;
-    private int maxBullets = 16;
-
-    private float range = 10f;
-    public LayerMask ignoreMe;
-
-    public GameObject bullets;
-    public GameObject gunBarrel;
-    public BulletManager manager;
-
-    private RaycastHit Hit;
-    Ray ray;
-
-    private void Start()
-    {
-        currentBullets = maxBullets;
-        Upgrade(BasicShoot);
-    }
-
-
-    private void Fire()
-    {
-        Shoot.Invoke();
-    }
-
-    private void BasicShoot()
-    {
-        Vector3 infront = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, infront, out Hit, range, ~ignoreMe))
-        {
-            manager.Bullets();
-        }
-    }
-    public void Upgrade(Action Upgrade)
-    {
-        Shoot += Upgrade;
-    }
-    private void BehindShot()
-    {
-
-    }
-
-    private void DoubleBullets()
-    {
-
-    }
-
-
-
-    // Pierce (ramping up damage?)
-    // Shoot forward and bacward
-    // ricocheting bullets (bouncy bullets )
-    // upgrade damage?
-    // Explosive ammo?
-    // increase ammo count?
-    // increase ammo size
-    // Shoot twice
-
-
-
-
-
-
-
-
-    //Blakes Demo code
-    /*
-     using AbilitySystem;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
 
 [RequireComponent(typeof(WeaponAbilityComponent))]
-public class ThisClassMeansThisObjectCanAttack : MonoBehaviour
+public class AttackController : MonoBehaviour
 {
     private WeaponAbilityComponent weapon;
 
-    BulletClass bull;
+    [SerializeField] private BulletClass bull;
 
     private void Awake()
     {
@@ -104,46 +19,10 @@ public class ThisClassMeansThisObjectCanAttack : MonoBehaviour
 
 
         BulletClass bullet = Instantiate(bull);
-        bullet.OnInstantiation();
+        bullet.OnInstantiation(transform);
 
-        abilityComponent.Use(bullet);
+        weapon.Use(bullet);
     }
 
-
-}
-
-
-
-
-public class BulletClass : MonoBehaviour
-{
-
-    public Vector3 direction;
-    public float damage;
-    public BulletModifier[] modifiers;
-    public LayerMask hittableLayer;
-
-    public void OnInstantiation(Transform fromTransform, BulletModifier[] mods = null)
-    {
-        this.transform.rotation = fromTransform.rotation;
-        direction = transform.eulerAngles;
-
-        if (mods != null)
-        {
-            modifiers = mods;
-        }
-    }
-
-
-    private void OnDisable()
-    {
-        modifiers = null;
-        hittableLayer = 0;
-        damage = 0;
-        direction = Vector3.zero;
-    }
-}
-
-     */
 
 }
---
+++ GGJ2022/Assets/Scripts/Level Management/RoomManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManager
+{
+    public class RoomManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Stores all parent objects of rooms, serialized to editor
+        /// </summary>
+        [SerializeField] private LevelMaster[] roomParents;
+
+
+        /// <summary>
+        /// ID for currently loaded level
+        /// </summary>
+        private int currentRoomID = -1;
+
+
+        /// <summary>
+        /// Unloads the current room and loads in the next
+        /// </summary>
+        /// <param name="index"></param>
+        public void LoadRoom(int index)
+        {
+            roomParents[currentRoomID].Load();
+
+            roomParents[index].Unload();
+            currentRoomID = index;
+        }
+
+    }
+}
